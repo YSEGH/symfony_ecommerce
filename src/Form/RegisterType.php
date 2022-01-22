2868d76fbe3d83959834df554021417d95c9739c
youssef <youssef.segh@hotmail.fr>
2022-01-22 11:17:54 +0100

    Register Controller

---
+++ src/Form/RegisterType.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Length;
+
+class RegisterType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('firstname', TextType::class, [
+                'label' => "Prénom",
+                'attr' => [
+                    'placeholder' => 'Merci de saisir votre prénom'
+                ]
+            ])
+            ->add('lastname', TextType::class, [
+                'label' => "Nom",
+                'attr' => [
+                    'placeholder' => 'Merci de saisir votre nom'
+                ]
+            ])
+            ->add('email', EmailType::class, [
+                'label' => "Email",
+                'attr' => [
+                    'placeholder' => 'Merci de saisir votre email'
+                ]
+            ])
+            ->add('password', RepeatedType::class,[
+                'type' => PasswordType::class,
+                'invalid_message' => 'Merci de saisir deux mots de passe identiques.',
+                'label' => "Mot de passe",
+                'required' => true,
+                'first_options' => [
+                    'label' => "Mot de passe",
+                    'attr' => [
+                    'placeholder' => 'Merci de saisir votre mot de passe'
+                ]
+                ],
+                'second_options' => [
+                    'label' => 'Confirmation du mot de passe',
+                    'attr' => [
+                    'placeholder' => 'Merci de confirmer votre mot de passe'
+                ]
+                ],
+            ])
+            ->add('submit', SubmitType::class, [
+                'label' => "S'inscrire",
+                
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
